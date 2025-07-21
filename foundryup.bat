e6b0e9926ce9948fa80ccaa5b1e9c45dce0d7f8e
Juan Blanco <juanfranblanco@hotmail.com>
2025-07-21 10:40:55 +0100

    Update foundryup.bat

--- foundryup.bat
+++ foundryup.bat
@@ -1,8 +1,8 @@
 @echo off
 IF [%~1] == [-v] GOTO version
-cargo install --git https://github.com/foundry-rs/foundry foundry-cli anvil forge cast --bins --locked
+cargo install --git https://github.com/foundry-rs/foundry foundry-cli anvil forge cast chisel --bins --locked
 GOTO done
 :version
-cargo install --git https://github.com/foundry-rs/foundry foundry-cli anvil forge cast --bins --locked --version %2
+cargo install --git https://github.com/foundry-rs/foundry foundry-cli anvil forge cast chisel --bins --locked --version %2
 
 :done
